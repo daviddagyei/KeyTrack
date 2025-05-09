5e56fcfd23001f3cb4e807948a85e26888b43c12
daviddagyei <iamdankwa@David.>
2025-05-09 15:59:36 -0500

    updated app.py

--- webapp/app.py
+++ webapp/app.py
@@ -144,4 +144,8 @@ def create_webapp():
 
 if __name__ == '__main__':
     app = create_webapp()
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
